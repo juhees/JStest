e7d19151aaabfbdaa18304288c6673062ceb42d2
JuheeShin <103228093+juhees@users.noreply.github.com>
2022-10-13 09:56:56 +0000

    변수, 자료형

--- public/js/let,const.js
+++
@@ -1,3 +0,0 @@
-let name = "mike";
-
-//let을 수정하고 싶을땐 let을 생략하고 name= "google";이런식으로 적어주면 됨
---
+++ public/js/자료형.js
@@ -0,0 +1,31 @@
+//-----자료형-----
+
+//문자형
+const name = "mike"; //문자형 String
+const age = 35;
+
+//문자형을 작성하는 세가지 방식
+const name1 = "mike";
+const name2 = 'mike';
+const name3 = `mike`;
+
+//큰따옴표와 작은따옴표는 큰 차이가 없다.
+const message = "I'm a boy.";
+const message2 = 'I\'m a boy.';
+
+const message3 = "my name is ${name}";
+
+const message4 = '나는 ${38+1}살 입니다';
+console.log(message4);
+
+
+//-----숫자형-----
+
+const age = 30; //숫자형 Number
+const PI = 3.14;
+
+console.log(1 + 2); //더하기
+console.log(1 + 2); //빼기
+console.log(1 + 2); //*곱하기
+console.log(1 + 2); ///나누기
+console.log(1 + 2); //%나머지
